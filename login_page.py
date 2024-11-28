b8264051966aa92d99e555e590d56cb1fd52ab23
Kunozuri <134244080+Kunozuri@users.noreply.github.com>
2024-11-28 23:04:24 +0800

    login_page.py

--- login_page.py
+++ login_page.py
@@ -1,34 +1,35 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QCheckBox
+from PyQt5.QtWidgets import (QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QCheckBox)
 
 class Login(QWidget):
-    def __init__(self, landing_page: object=None, parent = None):
+    def __init__(self, landing_page: object= None, admin: object= None, parent= None):
         super(Login, self).__init__(parent)
         
-        self.landing_page = landing_page # composition lang baby di kaya ng inheritance
+        self.__landing_page = landing_page # composition
+        self.__admin = admin
         
         self.settings()
         self.initUI()
         
     def initUI(self):
         
-        mini_widget = self.landing_page.custom_widget(width=700, height=500)
-        
         master_layout = QVBoxLayout()
         last_row = QHBoxLayout()
         
-        back_button = self.back_button()
-        back_button.clicked.connect(self.back_button_clicked)
+        back = self.back_button()
+        back.clicked.connect(self.back)
         
-        logo = self.landing_page.button001(size=50, font="Lato")
-        notice = self.landing_page.text_label("refrain from using your real password as I didnt put much attention into security", color="000000", size=12)
-        username = self.landing_page.input_box("Username", bg_color="#FFFFFF", width=500, height=70)
-        password = self.landing_page.input_box("Password", bg_color="#FFFFFF", is_password=True, width=500, height=70)
+        logo = self.__landing_page.button001(size=50, font="Lato")
+        notice = self.__landing_page.text_label("refrain from using your real password as I didnt put much attention into security", color="000000", size=12)
+        username = self.__landing_page.input_box("Username", bg_color="#FFFFFF", width=500, height=70)
+        password = self.__landing_page.input_box("Password", bg_color="#FFFFFF", is_password=True, width=500, height=70)
         remember_me = QCheckBox("Remember me")
-        forgot_password = self.landing_page.button001("Forgot password?")
-        login = self.landing_page.button002(text="Login", bg_color="#000000", text_color="white")
+        forgot_password = self.__landing_page.button001("Forgot password?")
+        login = self.__landing_page.button002(text="Login", bg_color="#000000", text_color="white")
         
-        master_layout.addWidget(back_button)
+        login.clicked.connect(self.login)
+        
+        master_layout.addWidget(back)
         master_layout.addWidget(logo, alignment=Qt.AlignmentFlag.AlignCenter)
         master_layout.addWidget(notice, alignment=Qt.AlignmentFlag.AlignCenter)
         master_layout.addWidget(username, alignment=Qt.AlignmentFlag.AlignCenter)
@@ -58,6 +59,10 @@ class Login(QWidget):
         
         return this_button
     
-    def back_button_clicked(self):
+    def back(self):
         self.hide()
-        self.landing_page.show()
+        self.__landing_page.show()
+    
+    def login(self):
+        self.hide()
+        self.__admin.show()
